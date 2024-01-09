76d5c00078e469a0b86b6560ddb5e11bd4a5e5b1
JacomoSeminara <12seminaraj@student.smchull.org>
2024-01-09 10:49:41 +0000

    Jacomo Tue p2

---
+++ Prod_em_on_Team1/Sprite.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prod_em_on_Team1
+{
+    internal class Sprite
+    {
+        protected Texture2D _texture;
+        public Sprite()
+        { }
+        public Sprite(Texture2D texture)
+        {
+            _texture = texture;
+        }
+
+        public virtual void Update()
+        {
+
+        }
+    }
+}
