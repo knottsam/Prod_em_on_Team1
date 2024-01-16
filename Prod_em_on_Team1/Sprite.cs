97badaf016bb7eb83594ca8d3e7e362fc94d6a3f
12CokanalagiB <12CokanalagiB@SMC-BITE2-06>
2024-01-16 11:11:53 +0000

    First Push!

---
+++ Prod_em_on_Team1/Sprite.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Prod_em_on_Team1
+{
+    internal class Sprite
+    {
+        protected Texture2D _texture;
+        public Sprite()
+        { }
+        public Sprite(Texture2D texture)
+        {
+            _texture = texture;
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, Position, Color.White);
+        }
+
+        public Vector2 Position
+        {
+            get; set;
+        }
+
+        public Rectangle Rectangle
+        {
+            get; set;
+        }
+
+        public Texture2D Texture
+        {
+            get { return _texture; }
+        }
+        public int Lane
+        {
+            get; set;
+        }
+    }
+}
